78b96370ad0dcda06de5643d2f91e325695b3c38
alok0511 <30834567+alok0511@users.noreply.github.com>
2019-01-24 09:50:26 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,27 @@
+package com.capgemini.beans;
+
+public class Student {
+	private int rollNo;
+	private String name;
+	private Course[] course;
+	public int getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Course[] getCourse() {
+		return course;
+	}
+	public void setCourse(Course[] course) {
+		this.course = course;
+	}
+	
+
+}
